282e6a0d5ce87a1a05f2c971d8dac331612db2b3
lucas <lucasmartinscaet@hotmail.com>
2022-09-29 17:22:45 -0300

    Testando escopo com for

--- scripts/ola.js
+++ scripts/ola.js
@@ -1,9 +1,25 @@
-var idade = 20
-var maiorIdade = (idade >= 18);
 
-if (maiorIdade){
-    console.log("Maior de idade");
-} else {
-    console.log("Menor de idade");
+// for(let i = 0;i < 11;i++){
+//     const varLocal = "Estou localmente";
+//     let varLocal2 = "Também estou localmente";
+//     var varGlobal = "Sou global";
+//     console.log(i);
+// }
+
+// console.log(varGlobal); //Funciona
+// // console.log(varLocal); //Não funciona
+// // console.log(varLocal2);//Não funciona
+
+
+//-----------------
+
+let msg = "Olá";
+const outra_msg = "Mensagem";
+
+if (msg){
+    console.log(msg); //Funciona
+    console.log(outra_msg); //Funciona
+    msg = "Outra coisa";
 }
 
+console.log(msg);
